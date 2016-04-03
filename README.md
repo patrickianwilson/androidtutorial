5610a8b1d154130735444aa61a21f60e0d40d5a6
patrickianwilson <patrick.ian.wilson@gmail.com>
2016-04-03 14:24:59 -0700

    lesson 1

--- README.md
+++
@@ -1,2 +0,0 @@
-# androidtutorial
-# androidtutorial
---
+++ src/main/java/com/patrickwilson/example/android/helps/WeatherService.java
@@ -0,0 +1,9 @@
+package com.patrickwilson.example.android.helps;
+
+/**
+ * Created by pwilson on 3/27/16.
+ */
+public class WeatherService {
+
+
+}
